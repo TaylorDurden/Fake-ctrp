3afc1baa19079d267b78c3efb2ae9391b8f26899
Taylor Li <258266864@qq.com>
2022-09-13 23:16:45 +0800

    3-9 【应用】添加初始化数据

--- Fake-ctrip.API/Database/AppDbContext.cs
+++ Fake-ctrip.API/Database/AppDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Fake_ctrip.API.Models;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Reflection;
 
 namespace Fake_ctrip.API.Database
 {
@@ -11,5 +13,30 @@ namespace Fake_ctrip.API.Database
 
         public DbSet<TouristRoute> TouristRoutes { get; set; }
         public DbSet<TouristRoutePicture> TouristRoutePictures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<TouristRoute>().HasData(new TouristRoute()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Title = "title title",
+            //    Description = "desc",
+            //    OriginalPrice = 0,
+            //    Features = "Features Features Features",
+            //    Fees = "Fees Fees Fees",
+            //    Notes = "Notes Notes Notes",
+            //    CreateTime = DateTime.UtcNow
+            //});
+            var touristRoutesMockData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutesMockData.json");
+            IList<TouristRoute> touristRoutes = JsonConvert.DeserializeObject<IList<TouristRoute>>(touristRoutesMockData);
+            modelBuilder.Entity<TouristRoute>().HasData(touristRoutes);
+
+            var touristRoutePicturesMockData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutePicturesMockData.json");
+            IList<TouristRoutePicture> touristRoutePictures = JsonConvert.DeserializeObject<IList<TouristRoutePicture>>(touristRoutePicturesMockData);
+            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);
+
+            base.OnModelCreating(modelBuilder);
+        }
+        //}
     }
 }
---
+++ Fake-ctrip.API/Migrations/20220913144627_DataSeeding.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fake_ctrip.API.Migrations
+{
+    public partial class DataSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TouristRoutes",
+                columns: new[] { "Id", "CreateTime", "DepartureTime", "Description", "DiscountPresent", "Features", "Fees", "Notes", "OriginalPrice", "Title", "UpdateTime" },
+                values: new object[] { new Guid("ed65d81f-a2ba-4c66-b29e-9e613b1c967d"), new DateTime(2022, 9, 13, 14, 46, 26, 858, DateTimeKind.Utc).AddTicks(6167), null, "desc", null, "Features Features Features", "Fees Fees Fees", "Notes Notes Notes", 0m, "title title", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TouristRoutes",
+                keyColumn: "Id",
+                keyValue: new Guid("ed65d81f-a2ba-4c66-b29e-9e613b1c967d"));
+        }
+    }
+}
